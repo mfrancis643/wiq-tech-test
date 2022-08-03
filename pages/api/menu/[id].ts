013a1f7782e8b84fd91ae90ca67c09ecf40fd3a6
mfrancis643 <mfrancis643@googlemail.com>
2022-08-03 12:59:50 +0100

    fetching menu from json - does'nt work right now

--- pages/api/menu/[id].ts
+++ pages/api/menu/[id].ts
@@ -1,51 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-
-import { Product } from '../../types';
+import { Menu, Product } from '../../types';
 
 export type ProductResponse = {
   products?: Array<Product>;
   error?: string;
 };
 
-export default function handler(
+let json = require('./data.json')
+
+
+
+const menus: Menu[] = json.menus
+console.log('menus')
+console.log(menus)
+
+
+export default (
   req: NextApiRequest,
   res: NextApiResponse<ProductResponse>
-) {
+) => {
+
+  const {id} = req.query
+
   try {
     res.status(200).json({
-    products: [
-      {
-        id: 1,
-        name: 'Bread',
-        description: 'Delicious bread with olive oil.',
-        price: 340,
-      },
-      {
-        id: 2,
-        name: 'Croquettes',
-        description: 'Croquettes with ham and cheese and stuff.',
-        price: 520,
-      },
-      {
-        id: 3,
-        name: 'Soup',
-        description: 'Soup of the day, ask the waiter I guess.',
-        price: 450,
-      },
-      {
-        id: 4,
-        name: 'Olives',
-        description: "Just olives, if you're into that kind of thing.",
-        price: 330,
-      },
-      {
-        id: 5,
-        name: 'Calamari',
-        description: "It's like breaded squid.",
-        price: 590,
-      },
-    ],
+    products: menus[id]
     });
   }
   catch (e){
